f111a365f2b7e66628ba2b5f067e6953d24322c1
Fredrik Østrem <frx.apps@gmail.com>
2017-07-11 14:03:13 +0200

    Stop on error

--- index.js
+++ index.js
@@ -5,33 +5,47 @@ module.exports = {
     const promise = new Promise((resolve, reject) => {
       stream.setEncoding('utf8');
 
+      let completed = false;
       let data = '';
 
       stream.on('data', (chunk) => {
-        data += chunk;
+        if(completed)
+          return;
 
-        let m;
-        do {
-          m = /\n\.\.\.([^\n]*)\n/.exec(data);
-          if(m != null) {
-            const docdata = data.slice(0, m.index);
-            data = data.slice(m.index + m[0].length);
+        try {
+          data += chunk;
 
-            result = yaml.safeLoad(docdata);
-            callback(result);
-          }
-        } while (m != null);
+          let m;
+          do {
+            m = /\n\.\.\.([^\n]*)\n/.exec(data);
+            if(m != null) {
+              const docdata = data.slice(0, m.index);
+              data = data.slice(m.index + m[0].length);
+
+              result = yaml.safeLoad(docdata);
+              callback(result);
+            }
+          } while (m != null);
+          
+        } catch (error) {
+          completed = true;
+          reject(error);
+        }
       });
 
       stream.on('end', () => {
-        const result = yaml.safeLoad(data);
+        completed = true;
 
+        const result = yaml.safeLoad(data);
         if(typeof result !== 'undefined')
           callback(result);
 
         resolve();
       });
-      stream.on('error', (error) => { reject(error); });
+      stream.on('error', (error) => {
+        completed = true;
+        reject(error);
+      });
     });
 
     promise.then(() => typeof done === 'function' && done(), (err) => done(err));
